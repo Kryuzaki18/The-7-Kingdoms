1563240717d32e0489a4582e709312e3be78d91b
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-06-25 01:35:12 +0800

    chore: add routes

--- api-the-7-kingdoms/src/config/app-routes.ts
+++ api-the-7-kingdoms/src/config/app-routes.ts
@@ -1,3 +1,18 @@
+const API_BASE_URL = 'https://anapioficeandfire.com/api';
+
 export const ROUTES = {
   HEALTH: "/health",
+  AUTH: {
+    ME: 'me',
+    LOGIN: 'login',
+    SIGNUP: 'signup',
+    LOGOUT: 'logout',
+  }
 };
+
+export const EXT_ROUTES = {
+  ALL_BOOKS: `${API_BASE_URL}/books`,
+  BOOKS: (id: number) => `${API_BASE_URL}/books/${id}`,
+  CHARACTERS: (id: number) => `${API_BASE_URL}/characters/${id}`,
+  HOUSES: (id: number) => `${API_BASE_URL}/houses/${id}`,
+}
