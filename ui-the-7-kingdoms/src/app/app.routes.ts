70237510264f507082cc30eec791ce4b31cb2a84
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-06-25 03:23:06 +0800

    chore: update routes

--- ui-the-7-kingdoms/src/app/app.routes.ts
+++ ui-the-7-kingdoms/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
+
 import { AuthComponent } from './features/auth/auth.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { SignupComponent } from './features/auth/signup/signup.component';
+import { HomeComponent } from './features/home/home.component';
+import { CharactersComponent } from './features/characters/characters.component';
+import { HousesComponent } from './features/houses/houses.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +15,14 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
       { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'characters', component: CharactersComponent },
+      { path: 'houses', component: HousesComponent },
     ],
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
