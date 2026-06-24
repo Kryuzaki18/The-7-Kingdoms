7796b6be90b492a670f921cb44b09f01dd9bf2ae
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-06-25 02:56:28 +0800

    chore: rename folders

--- ui-the-7-kingdoms/src/app/features/auth/auth.component.ts
+++ ui-the-7-kingdoms/src/app/features/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login.component/login.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-auth',
