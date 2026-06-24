6a99019a799f4b5d6700c75d70f18f032b938ca4
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-06-25 03:18:43 +0800

    chore: add houses

---
+++ ui-the-7-kingdoms/src/app/features/houses/houses.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HousesComponent } from './houses.component';
+
+describe('HousesComponent', () => {
+  let component: HousesComponent;
+  let fixture: ComponentFixture<HousesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HousesComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HousesComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ui-the-7-kingdoms/src/app/features/houses/houses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houses.component',
+  imports: [],
+  templateUrl: './houses.component.html',
+  styleUrl: './houses.component.scss',
+})
+export class HousesComponent {}
