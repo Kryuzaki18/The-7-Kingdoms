4f59f18a136a9a1e9b172de084c87512d8b1190e
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-06-27 01:02:57 +0800

    chore: deployment setup

---
+++ ui-the-7-kingdoms/proxy.conf.js
@@ -0,0 +1,14 @@
+const https = require('https');
+
+const agent = new https.Agent({ keepAlive: true, maxSockets: 25 });
+agent.setMaxListeners(25);
+
+module.exports = {
+  '/api': {
+    target: 'https://api-the-7-kingdoms.onrender.com',
+    changeOrigin: true,
+    secure: true,
+    agent,
+    logLevel: 'warn',
+  },
+};
